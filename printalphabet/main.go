ee22521aa50ae3e018ffa9d1c64de8032a125c31
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-10-14 09:44:58 +0200

    x

---
+++ printalphabet/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	for compteur := 97; compteur < 123; compteur++ {
+		z01.PrintRune(rune(compteur))
+
+	}
+
+	z01.PrintRune(rune('\n'))
+}
