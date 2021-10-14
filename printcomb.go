03e7acdd7b550880ce369388e1c63933341fe381
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-10-14 14:17:24 +0200

    4e exercice

---
+++ printcomb.go
@@ -0,0 +1,30 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for a := '0'; a <= '7'; a++ {
+		for b := a + 1; b <= '8'; b++ {
+			for c := b + 1; c <= '9'; c++ {
+				if a < b {
+					if b < c {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(c)
+						if a == '7' && b == '8' && c == '9' {
+							z01.PrintRune(10)
+						} else {
+							z01.PrintRune(44)
+							z01.PrintRune(32)
+						}
+
+					}
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	PrintComb()
+}
