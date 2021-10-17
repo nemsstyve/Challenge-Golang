1502f923bc77d003d3e91ea9f83ac80030dc8337
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-10-17 17:55:04 +0200

    iterative

---
+++ printnbr.go
@@ -0,0 +1,22 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintNbr(n int) {
+
+	t := 1
+	if n < 0 {
+		t = -1
+		z01.PrintRune('-')
+	}
+	if n != 0 {
+		f := (n / 10) * t
+		if f != 0 {
+			PrintNbr(f)
+		}
+		k := (n % 10 * t) + '0'
+		z01.PrintRune(rune(k))
+	}
+}
