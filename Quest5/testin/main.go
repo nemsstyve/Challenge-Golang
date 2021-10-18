48def0d888a47184f6b88e66604af1ab7f0e134c
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-10-18 17:31:49 +0200

    index

---
+++ Quest5/testin/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.Index("Hello!", "l"))
+	fmt.Println(piscine.Index("Salut!", "alu"))
+	fmt.Println(piscine.Index("Ola!", "hOl"))
+}
