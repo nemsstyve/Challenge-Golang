8635acd2c7dda56276618aefb89a4f4d8cb2bd71
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-10-19 17:03:35 +0200

    isalpha

---
+++ Quest5/testisa/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.IsAlpha("Hello! How are you?"))
+	fmt.Println(piscine.IsAlpha("HelloHowareyou"))
+	fmt.Println(piscine.IsAlpha("What's this 4?"))
+	fmt.Println(piscine.IsAlpha("Whatsthis4"))
+}
