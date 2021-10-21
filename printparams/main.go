a7a6ea39fe0fbe28a5033f1cb06a0d6cc3a6d839
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-10-21 16:11:56 +0200

    printpa

---
+++ printparams/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arg := os.Args
+	for i, s := range arg {
+		if i != 0 {
+			for _, a := range s {
+				z01.PrintRune(a)
+			}
+			z01.PrintRune('\n')
+		}
+	}
+
+}
