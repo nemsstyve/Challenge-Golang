6d53d732aa00bb02e8f308b2c6753310745839a9
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-10-22 12:16:52 +0200

    sortparams

---
+++ sortparams/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+
+	count := 0
+	for range os.Args {
+		count++
+	}
+	for i := 1; i < count-1; i++ {
+		for j := i + 1; j < count; j++ {
+			if os.Args[i] > os.Args[j] {
+				temp := os.Args[j]
+				os.Args[j] = os.Args[i]
+				os.Args[i] = temp
+			}
+		}
+	}
+	i := 1
+	for i < count {
+		out := []rune(os.Args[i])
+		for _, j := range out {
+			z01.PrintRune(j)
+		}
+		z01.PrintRune(10)
+		i++
+	}
+}
