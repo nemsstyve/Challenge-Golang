b888e8c71ce6ad911ba8b8ee1d537517bc114834
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-10-26 09:09:50 +0200

    nbrconvert

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -1,57 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"os"
-
-	"github.com/01-edu/z01"
+	"strconv"
+	"unicode"
 )
 
+func IsNumeric(s string) bool {
+	for _, r := range s {
+		if !unicode.IsDigit(r) {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	arg := os.Args[1:]
-	len := 0
-	flag := false
-	for i := range arg {
-		len = i
-	}
-	for _, val := range arg {
-		if val == "--upper" {
-			flag = true
-		}
-	}
-	if flag {
-		for i := 0; i <= len; i++ {
-			z01.PrintRune(ToUpper(ConvertToLetter(arg[i])))
-		}
-		z01.PrintRune(10)
-	} else if len == 0 {
-		z01.PrintRune(10)
-	} else {
-		for i := 0; i <= len; i++ {
-			z01.PrintRune(ConvertToLetter(arg[i]))
-		}
-		z01.PrintRune(10)
-	}
-}
+	arguments := os.Args[1:]
+	for i := range arguments {
+		if IsNumeric(arguments[i]) {
+			number, _ := strconv.Atoi(arguments[i])
+			boole := false
+			if os.Args[1] == "--upper" {
+				boole = true
+			}
 
-func ConvertToLetter(s string) rune {
-	numb := 0
-	for _, i := range s {
-		count := 0
-		for k := '0'; k < i; k++ {
-			count++
+			if number <= 26 && number >= 1 && !boole {
+				number += 96
+				fmt.Printf("%c", rune(number))
+			} else if number <= 26 && number >= 1 && boole {
+				number += 64
+				fmt.Printf("%c", rune(number))
+			} else {
+				fmt.Print(" ")
+			}
+		} else {
+			if !(arguments[i] == "--upper" && i == 0) {
+				fmt.Print(" ")
+			}
 		}
-		numb = numb*10 + count
 	}
-	numb = numb + 96
-	if numb >= 123 {
-		return ' '
-	}
-	return rune(numb)
-}
-
-func ToUpper(r rune) rune {
-	if r-32 < 65 {
-		return r
-	}
-	return r - 32
+	fmt.Println()
 }
