b0e61171accf52d30bac04533edc1364dbe76ffe
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-01 15:07:36 +0100

    splitwhite

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,38 +1,28 @@
 package piscine
 
 func SplitWhiteSpaces(str string) []string {
-	var arrayString []string
-	countWords := 1
-	lenStr := 0
-	result := ""
-	for c := range str {
-		if isWhiteSpace(str[c]) {
-			countWords++
-		}
-		lenStr++
-	}
-	arrayString = make([]string, countWords)
-	i := 0
-	for j, c := range str {
-		if j+1 == lenStr {
-			arrayString[i] = result + string(str[j])
-		}
-		if isWhiteSpace(str[j]) {
-			if i <= countWords {
-				arrayString[i] = result
-				i++
-				result = ""
-			}
-		} else {
-			result += string(c)
-		}
-	}
-	return arrayString
-}
 
-func isWhiteSpace(r byte) bool {
-	if r == ' ' || r == '\n' || r == '\t' {
-		return true
+	TextToString := ""
+	t := []string{}
+	for i, v := range str {
+		if i == lent2(str)-1 && string(v) != " " && string(v) != "\t" && string(v) != "\n" {
+			TextToString += string(v)
+			t = append(t, TextToString)
+		} else if string(v) != " " && string(v) != "\t" && string(v) != "\n" {
+			TextToString += string(v)
+		} else {
+			if len(TextToString) >= 1 {
+				t = append(t, TextToString)
+			}
+			TextToString = ""
+		}
 	}
-	return false
+	return t
+}
+func lent2(d string) int {
+	inc := 0
+	for _, _ = range d {
+		inc++
+	}
+	return inc
 }
