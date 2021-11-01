292e737cf5a539cbe368187a2a4847cb5e3f46a0
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-01 21:24:46 +0100

    isprintable

---
+++ Quest5/testisp/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.IsPrintable("Hello"))
+	fmt.Println(piscine.IsPrintable("Hello\n"))
+
+}
---
+++ isprintable.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IsPrintable(str string) bool {
+	h := []rune(str)
+
+	for i := 0; i <= len(h)-1; i++ {
+		if (h[i] >= 0) && (h[i] <= 31) {
+			return false
+		}
+
+	}
+	return true
+}
