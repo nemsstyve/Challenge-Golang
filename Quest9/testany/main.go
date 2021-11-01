846c665c5f2ad41160759a506e188e952d579105
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-01 21:28:32 +0100

    any

--- Quest9/testany/main.go
+++ Quest9/testany/main.go
@@ -9,8 +9,8 @@ func main() {
 	a1 := []string{"Hello", "how", "are", "you"}
 	a2 := []string{"This", "is", "4", "you"}
 
-	result1 := piscine.Any(piscine.IsAlpha, a1)
-	result2 := piscine.Any(piscine.IsAlpha, a2)
+	result1 := piscine.Any(piscine.IsNumeric, a1)
+	result2 := piscine.Any(piscine.IsNumeric, a2)
 
 	fmt.Println(result1)
 	fmt.Println(result2)
