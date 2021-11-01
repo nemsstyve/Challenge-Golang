b3b71727b314d131949fca122da6d64d749b0ce0
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-01 21:20:39 +0100

    isnumeric

---
+++ Quest5/testisn/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.IsNumeric("010203"))
+	fmt.Println(piscine.IsNumeric("01,02,03"))
+}
---
+++ isnumeric.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IsNumeric(str string) bool {
+	h := []rune(str)
+
+	for i := 0; i <= len(h)-1; i++ {
+		if (h[i] >= 0) && (h[i] <= 47) || (h[i] >= 58) && (h[i] <= 127) {
+			return false
+		}
+
+	}
+	return true
+}
