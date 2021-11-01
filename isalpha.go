9613f90d82349cc95e0e89ecabc18aff640314e8
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-01 21:17:47 +0100

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -1,14 +1,11 @@
 package piscine
 
 func IsAlpha(str string) bool {
-
 	h := []rune(str)
-
 	for i := 0; i <= len(h)-1; i++ {
 		if (h[i] >= 0) && (h[i] <= 47) || (h[i] >= 58) && (h[i] <= 64) || (h[i] >= 91) && (h[i] <= 96) || (h[i] >= 123) && (h[i] <= 127) {
 			return false
 		}
-
 	}
 	return true
 }
