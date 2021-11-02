8c57d48bed4bf0785f59c89fb91f23f5377b918f
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-02 14:51:23 +0100

    listlast

---
+++ Quest11/testlast/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	link := &piscine.List{}
+	link2 := &piscine.List{}
+	piscine.ListPushBack(link, "three")
+	piscine.ListPushBack(link, 3)
+	piscine.ListPushBack(link, "1")
+	fmt.Println(piscine.ListLast(link))
+	fmt.Println(piscine.ListLast(link2))
+}
