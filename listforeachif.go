bb79ca6ad22df7d579108f5a9b46ab0468ef2ac1
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-02 16:33:45 +0100

    listforeachif

--- listforeachif.go
+++ listforeachif.go
@@ -10,22 +10,32 @@ func IsPositive_node(node *NodeL) bool {
 	return false
 }
 
-func IsAlNode(node *NodeL) bool {
+func IsNegative_node(node *NodeL) bool {
 	switch node.Data.(type) {
 	case int, float32, float64, byte:
 		return node.Data.(int) > 0
 	case string, rune:
 		return false
 	}
-	return true
+	return false
 }
 
-func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
-	novohead := l.Head
-	for novohead != nil {
-		if cond(novohead) == true {
-			f(novohead)
+func IsAlnode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return true
+	case string, rune:
+		return false
+	}
+	return false
+}
+
+func ListForEachIf(l *List, f func(*NodeL), comp func(*NodeL) bool) {
+	it := l.Head
+	for it != nil {
+		if comp(it) {
+			f(it)
 		}
-		novohead = novohead.Next
+		it = it.Next
 	}
 }
