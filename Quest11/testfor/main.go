db044a81893afdd3f69435be55a5b2d77dc551b8
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-02 16:02:26 +0100

    listforeach

---
+++ Quest11/testfor/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	link := &piscine.List{}
+	piscine.ListPushBack(link, "1")
+	piscine.ListPushBack(link, "2")
+	piscine.ListPushBack(link, "3")
+	piscine.ListPushBack(link, "5")
+	piscine.ListForEach(link, piscine.Add2_node)
+	it := link.Head
+	for it != nil {
+		fmt.Println(it.Data)
+		it = it.Next
+	}
+}
