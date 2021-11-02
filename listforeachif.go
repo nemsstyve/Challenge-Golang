a661214df6effc8872454be1f8db7196e5fea78e
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-02 16:29:43 +0100

    listforeachif

--- listforeachif.go
+++ listforeachif.go
@@ -10,22 +10,12 @@ func IsPositive_node(node *NodeL) bool {
 	return false
 }
 
-func IsNegative_node(node *NodeL) bool {
+func IsAlNode(node *NodeL) bool {
 	switch node.Data.(type) {
 	case int, float32, float64, byte:
 		return node.Data.(int) > 0
 	case string, rune:
 		return false
-	}
-	return false
-}
-
-func IsNotNumeric_node(node *NodeL) bool {
-	switch node.Data.(type) {
-	case int, float32, float64, byte:
-		return false
-	case string, rune:
-		return true
 	}
 	return true
 }
