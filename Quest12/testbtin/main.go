61de0464e2287c1ab86796d5f2fa2ca0cd17db37
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-04 14:31:29 +0100

    btreeinserdata

---
+++ Quest12/testbtin/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	root := &piscine.TreeNode{Data: "4"}
+	piscine.BTreeInsertData(root, "1")
+	piscine.BTreeInsertData(root, "7")
+	piscine.BTreeInsertData(root, "5")
+	fmt.Println(root.Left.Data)
+	fmt.Println(root.Data)
+	fmt.Println(root.Right.Left.Data)
+	fmt.Println(root.Right.Data)
+
+}
