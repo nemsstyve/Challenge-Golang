f745317f696764855d67aa5772235e15401bd08a
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-04 11:23:09 +0100

    point

--- point/main.go
+++ point/main.go
@@ -1,19 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
-type point struct {
-	x int
-	y int
-}
-
-func setPoint(ptr *point) {
-	ptr.x = 42
-	ptr.y = 21
+func setPoint(x *int, y *int) {
+	*x = 42
+	*y = 21
 }
 
 func main() {
-	points := &point{}
-	setPoint(points)
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	x := 42
+	y := 21
+	setPoint(&x, &y)
+	z01.PrintRune(rune('x'))
+	z01.PrintRune(rune(' '))
+	z01.PrintRune(rune('='))
+	z01.PrintRune(rune(' '))
+	z01.PrintRune(rune('4'))
+	z01.PrintRune(rune('2'))
+	z01.PrintRune(rune(','))
+	z01.PrintRune(rune(' '))
+	z01.PrintRune(rune('y'))
+	z01.PrintRune(rune(' '))
+	z01.PrintRune(rune('='))
+	z01.PrintRune(rune(' '))
+	z01.PrintRune(rune('2'))
+	z01.PrintRune(rune('1'))
+	z01.PrintRune(rune('\n'))
 }
+
+//func setPoint(ptr *point) {
+//	ptr.x = 42
+//	ptr.y = 21
+//}
+
+//func main() {
+//	points := &point{}
+//	setPoint(points)
+//	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+//}
