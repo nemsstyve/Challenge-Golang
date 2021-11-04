93a5557870faf76cb17759efe98ebbcfb5ea3f9a
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-04 15:00:51 +0100

    applypreorder

---
+++ Quest12/testbtpre/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	root := &piscine.TreeNode{Data: "4"}
+	piscine.BTreeInsertData(root, "1")
+	piscine.BTreeInsertData(root, "7")
+	piscine.BTreeInsertData(root, "5")
+	piscine.BTreeApplyPreorder(root, fmt.Println)
+}
