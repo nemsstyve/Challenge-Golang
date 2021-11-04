ea7ede6c26877e626020d253f5867e4190e53af6
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-04 16:02:47 +0100

    max

---
+++ Quest12/testbtmax/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	root := &piscine.TreeNode{Data: "4"}
+	piscine.BTreeInsertData(root, "1")
+	piscine.BTreeInsertData(root, "7")
+	piscine.BTreeInsertData(root, "5")
+	max := piscine.BTreeMax(root)
+	fmt.Println(max.Data)
+}
