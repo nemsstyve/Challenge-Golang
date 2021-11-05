3b48ee75f1b9811b428042a0d5d9fbca1052b4b5
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-05 09:01:02 +0100

    toupper

---
+++ Quest5/testto/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.ToUpper("Hello! How are you?"))
+}
---
+++ toupper.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ToUpper(s string) string {
+	h := []rune(s)
+	result := ""
+	for i := 0; i <= len(h)-1; i++ {
+
+		if (h[i] >= 'a') && (h[i] <= 'z') {
+
+			h[i] = h[i] - 32
+		}
+		result += string(h[i])
+	}
+	return result
+}
