b78be97cf03bf3268c7c247e9ba9925b9dfbf854
SNEMATCHA <styvechristopher.nematcha@ynov.com>
2021-11-08 10:26:02 +0100

    basicatoi2

---
+++ Quest3/testba/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.BasicAtoi2("12345"))
+	fmt.Println(piscine.BasicAtoi2("0000000012345"))
+	fmt.Println(piscine.BasicAtoi2("012 345"))
+	fmt.Println(piscine.BasicAtoi2("Hello World!"))
+}
---
+++ basicatoi2.go
@@ -0,0 +1,23 @@
+package piscine
+
+func check(s string) bool {
+	for _, c := range s {
+		if c == ' ' || c < '0' || c > '9' {
+			return false
+		}
+	}
+	return true
+}
+func BasicAtoi2(str string) int {
+	x := 0
+	if check(str) == true {
+		for _, c := range str {
+			cnt := 0
+			for i := '1'; i <= c; i++ {
+				cnt++
+			}
+			x = x*10 + cnt
+		}
+	}
+	return x
+}
